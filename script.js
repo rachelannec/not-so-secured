2b2d3ad76d008da0cc0e160413d2099e1fa59df3
Rachel Anne Cilon <rachelannecilon@gmail.com>
2024-12-24 15:36:59 +0800

    add ccoments, fix file links, create folder for contents

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ document.querySelector('#login-form form').addEventListener('submit', function(e
     event.preventDefault();
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
+    
 
-    let password1 = 'asdfghjkl';
-    let password2 = 'password';
+    // password for content pages
+    let password1 = 'password1';
+    let password2 = 'password2';
     if (password === password1) {
-        window.location.href = 'content-1.html'; // to redirect to the content-1 page ;-;  
+        window.location.href = '/content-pages/content-1.html'; // to redirect to the content-1 page ;-;  
     } else if (password === password2) {
-        window.location.href = 'content-2.html'; // to redirect to the content-2 page ;-;
+        window.location.href = '/content-pages/content-2.html'; // to redirect to the content-2 page ;-;
     } else {
         errorMessage.textContent = 'Wrong key! The contents on this page ain\'t for you. :>';
     }
 });
 
 function goBack() {
-    window.location.href = 'index.html';
+    window.location.href = '../index.html';
 }
